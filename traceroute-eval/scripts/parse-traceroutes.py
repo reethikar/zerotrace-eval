02bea6975c757e17a3315aa65632c9a1209ddb9c
fsiraj <fsiraj@umich.edu>
2023-03-07 15:34:12 -0500

    Added dublin parser

--- traceroute-eval/scripts/parse-traceroutes.py
+++ traceroute-eval/scripts/parse-traceroutes.py
@@ -8,13 +8,17 @@ data_root = "../data"
 tr_methods = ["udp", "icmp", "tcp", "paris", "dublin"]
 
 # JSON keys
-TS = "Timestamp"            # TODO: How to get this?
+TS = "Timestamp"
 DEST = "DestinationIP"
 TYPE = "TracerouteType"
 HOPS = "Hops"
 TTL = "TTL"
 IP = "IPs"
 RTT = "RTTs"
+
+# Regex patterns
+ip_pattern = re.compile(r"\d+\.\d+\.\d+\.\d+")
+ping_pattern = re.compile(r"\d+\.\d+")
 
 def base_parser(traceroute, method):
     """
@@ -26,24 +30,21 @@ def base_parser(traceroute, method):
     Returns:
         json_dict (dict): dict containing relevant information
     """
-    json_dict = {}
     lines = traceroute.split("\n")
 
-    ip_pattern = re.compile(r"\d+\.\d+\.\d+\.\d+")
-    ping_pattern = re.compile(r"\d+\.\d+")
-
+    json_dict = {}
     json_dict[TS] = lines[0]
     json_dict[DEST] = lines[1].split()[2]
     json_dict[TYPE] = method
     json_dict[HOPS] = []
 
-    # Each line contains an IP, latency, or * (failure)
-    for line in lines[2:-1]:
-        tokens = line.split()
+    # Each line represents one hop
+    for hop in lines[2:-1]:
+        tokens = hop.split()
         ttl = tokens[0]
         ips = []
         rtts = []
-
+        
         # A token is either an IP, latency, "ms", or "*"
         for token in tokens[1:]:
             if token == "*":
@@ -58,7 +59,7 @@ def base_parser(traceroute, method):
                     # Find last non-"*" IP
                     last_ip = [ip for ip in ips if ip != "*"][-1]
                     ips.append(last_ip)
-
+        
         # Add hop to data dictionary
         json_dict[HOPS].append({
             TTL: ttl, IP: ips, RTT: rtts
@@ -110,30 +111,69 @@ def paris_parser(traceroute):
     """
     return base_parser(traceroute, "paris")
 
-parsers = {"udp": udp_parser, "icmp": icmp_parser, "tcp": tcp_parser, "paris": paris_parser}
+def dublin_parser(traceroute):
+    """
+    Parses one ICMP traceroute.
+
+    Args: 
+        traceroute (str): the full traceroute json as a string
+    Returns:
+        json_dict (dict): dict containing relevant information
+    """
+    flow_dicts = []
+    traceroute = json.loads(traceroute)
+
+    # JSON is a dict with flow_id keys
+    for flow_id, flow in traceroute["flows"].items():
+        json_dict = {}
+        json_dict[TS] = flow[0]["sent"]["timestamp"]
+        json_dict[DEST] = flow[0]["sent"]["ip"]["dst"]
+        json_dict[TYPE] = "dublin"
+        json_dict[HOPS] = []
+        
+        # The value of the flow_id is a list of hop dictionaries
+        for hop in flow:
+            ttl = hop["sent"]["ip"]["ttl"]
+            try:
+                rtt = hop["rtt_usec"]/1000
+                ip = hop["received"]["ip"]["src"]
+            except TypeError:
+                rtt = "*"
+                ip = "*"
+
+             # Add hop to data dictionary
+            json_dict[HOPS].append({
+                TTL: ttl, IP: [ip if ip else "*"], RTT: [rtt if rtt else "*"]
+            })
+            
+        flow_dicts.append(json_dict)
+
+    return flow_dicts
+
+parsers = {"udp": udp_parser, "icmp": icmp_parser, "tcp": tcp_parser, "paris": paris_parser, "dublin": dublin_parser}
 
 if __name__ == "__main__":
 
-    # # Check data directories exist
-    # assert isdir(data_root), f"Root {data_root} does not exist."
-    # for tr in tr_methods:
-    #     assert isdir(join(data_root, tr)), f"Data directory {tr} does not exist."
+    # Check data directories exist
+    assert isdir(data_root), f"Root {data_root} does not exist."
+    for tr in tr_methods:
+        assert isdir(join(data_root, tr)), f"Data directory {tr} does not exist."
     
-    # # Load data (filenames)
-    # print("Checking for data...")
-    # filenames = {}
-    # for tr in tr_methods:
-    #     filenames[tr] = listdir(join(data_root, tr, "traceroutes"))
-    #     print(f"\tFound {len(filenames[tr])} {tr} traceroutes.")
+    # Load data (filenames)
+    print("Checking for data...")
+    filenames = {}
+    for tr in tr_methods:
+        filenames[tr] = listdir(join(data_root, tr, "traceroutes"))
+        print(f"\tFound {len(filenames[tr])} {tr} traceroutes.")
 
-    # # Parse data
-    # print("Parsing data...")
-    # for tr in tr_methods:
-    #     src_dir = join(data_root, tr, "traceroutes")
+    # Parse data
+    print("Parsing data...")
+    for tr in tr_methods:
+        src_dir = join(data_root, tr, "traceroutes")
 
-    with open("../sample-traceroutes/icmp.sample.txt") as f:
-        traceroute = f.read()
-        json_dict = parsers["icmp"](traceroute)
-        pprint.pprint(json_dict)
+    # with open("../sample-traceroutes/trace.json") as f:
+    #     traceroute = f.read()
+    #     json_dicts = dublin_parser(traceroute)
+    #     pprint.pprint(json_dicts[0])
 
     
