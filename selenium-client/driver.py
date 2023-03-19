0c6dedc16af9e6cb8e96548d4967b2ce2cbc19d4
Sam Korman <skorman@umich.edu>
2023-03-19 14:35:51 -0400

    Made vpn_loc required if using VPN

--- selenium-client/driver.py
+++ selenium-client/driver.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 import time
 import threading
 import click
@@ -51,7 +52,7 @@ class StartDriver:
             raise Exception("A valid browser must be selected")
         
         
-    def run_experiment(self, uniqname="skorman", network="UMich WiFi", vpn=False, vpn_loc="Michigan", your_loc="Ann Arbor, MI, USA"):
+    def run_experiment(self, uniqname="skorman", network="UMich WiFi", vpn=False, provider="IPVanish", vpn_loc="Michigan", your_loc="Ann Arbor, MI, USA"):
         self.driver.get("https://test.reethika.info/measure")
         time.sleep(0.5)
         # Fill uniqname field
@@ -77,6 +78,7 @@ class StartDriver:
         else:
             vpn_select_elem[1].click()
             time.sleep(0.3)
+            self.driver.find_element(By.NAME, "name_vpn").send_keys(provider)
             self.driver.find_element(By.NAME, "location_vpn").send_keys(vpn_loc)
             self.driver.find_element(By.NAME, "location_user").send_keys(your_loc)
             
@@ -88,10 +90,20 @@ class StartDriver:
         
         self.wait_for_completion()
         
+
+    def element_exists(self, elem_id):
+        try:
+            self.driver.find_element(By.ID, "status")
+        except NoSuchElementException:
+            return False
+        return True
+        
         
     def wait_for_completion(self):
         counter = 0
-        while self.driver.find_element(By.ID, "status").text != "Done" and counter < 60:
+        while not self.element_exists("status"):
+            time.sleep(0.25)
+        while self.driver.find_element(By.ID, "status").text != "Done" and counter < 100:
             counter += 1
             time.sleep(1)
             
@@ -107,6 +119,8 @@ def run_browser_test(browser, options):
 def run_all_browser_tests(browsers: list, options):
     threads: list[threading.Thread] = []
     for browser in browsers:
+        if browser == "safari":
+            time.sleep(4)
         browser_thread = threading.Thread(target=run_browser_test, args=[browser, options])
         browser_thread.start()
         threads.append(browser_thread)
@@ -118,10 +132,18 @@ def run_all_browser_tests(browsers: list, options):
 @click.option('-u', '--uniqname', default='skorman', help='Uniqname of person running test')
 @click.option('-n', '--network', default='UMich WiFi', help='Network connected to (e.g. UMich WiFi, Comcast Home, T-Mobile, Verizon)')
 @click.option('--vpn', is_flag=True, help='Indicate whether running through a VPN')
-@click.option('-l', '--vpn_loc', default='Ann Arbor, MI, USA', help='Location of VPN (City, State, Country)')
+@click.option('-p', '--provider', default="IPVanish", help="VPN Provider Name and Server")
+@click.option('-l', '--vpn_loc', help='Location of VPN (City, State, Country)')
 @click.option('-y', '--your_loc', default='Ann Arbor, MI, USA', help='Your location (City, State, Country)')
-def main(**kwargs):
-    allowable_browsers = ["safari", "chrome", "firefox"]
+@click.option('-s', '--safari', is_flag=True, help='Indicate whether to only run Safari')
+def main(safari=False, **kwargs):
+    allowable_browsers = ["chrome", "firefox", "safari"]
+    if safari:
+        allowable_browsers = ["safari"]
+        
+    if kwargs['vpn'] and not kwargs['vpn_loc']:
+        raise Exception('You must provide VPN server location using -l flag if using a VPN (Please enter "unknown" if you are unsure about your VPN server\'s location, or "best available" if you chose that in your VPN settings)')
+    
     run_all_browser_tests(allowable_browsers, kwargs)
 
 
